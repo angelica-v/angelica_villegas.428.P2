f6fa4f7672c3b0544d90c98afb6e028004d89638
angelica-v <angelicadv10@gmail.com>
2022-10-13 17:14:31 -0500

    adding new models & class time

---
+++ Assets/broomMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class broomMove : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (y >= 3)
+          {
+              y = y - speed * Time.deltaTime;
+          }
+          if (y < 3)
+          {
+              y = y + speed * Time.deltaTime;
+          }
+          apple.transform.position = new Vector3(0, y, 0);*/
+    }
+    
+}
