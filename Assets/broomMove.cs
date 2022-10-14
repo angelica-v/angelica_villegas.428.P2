f10471a07933523f07fb34ec519bbc921ab15ffb
angelica-v <angelicadv10@gmail.com>
2022-10-14 12:50:40 -0500

    moving brooms

--- Assets/broomMove.cs
+++ Assets/broomMove.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class broomMove : MonoBehaviour
 {
     private Vector3 broomStart;
-    private Vector3 broomMove;
 
     // Start is called before the first frame update
     void Start()
     {
-        broomStart = transform.position
+        broomStart = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        broomMove = new Vector3(0.0f, 0.35 * Mathf.Sin(Time.time), 0.0f);
-        transform.position = broomStart + broomMove; 
+        transform.position = broomStart + new Vector3(0.0f, 0.3f * Mathf.Sin(Time.time), 0.0f); 
     }
     
 }
